af26d989bea686fed35d8d9ead2cabdadef7071e
Michael Kaiser <micka@nuuday.dk>
2020-06-09 16:05:42 +0200

    fix: tf fmt

--- modules/nginx-ingress-controller/output.tf
+++ modules/nginx-ingress-controller/output.tf
@@ -1,15 +1,15 @@
 output "vpc_public_subnet_eks_ingress_tags" {
   description = "Tags for public subnets in the VPC to use for integration with EKS."
   value = {
-      "kubernetes.io/cluster/${var.cluster_name}" = "shared",
-      "kubernetes.io/role/elb" = "1"
-    }
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared",
+    "kubernetes.io/role/elb"                    = "1"
+  }
 }
 
 output "vpc_internal_subnet_eks_ingress_tags" {
   description = "Tags for internal subnets in the VPC to use for integration with EKS."
   value = {
-      "kubernetes.io/cluster/${var.cluster_name}" = "shared",
-      "kubernetes.io/role/internal-elb" = "1"
-    }
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared",
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 }

f66dbddf3925bdb367e0bfc635f90b0741ea2544
David Wolffberg <1350533+wolffberg@users.noreply.github.com>
2020-08-27 12:08:47 +0200

    chore: removal of unused variables

--- modules/calico/variables.tf
+++ modules/calico/variables.tf
@@ -10,11 +10,6 @@ variable "chart_version" {
   type        = string
 }
 
-variable "oidc_provider_issuer_url" {
-  description = "Issuer used in the OIDC provider associated with the EKS cluster to support IRSA."
-  type        = string
-}
-
 variable "create_namespace" {
   description = "Whether to create the namespace defined in the namespace variable. Will fail if the namespace already exists."
   default     = false

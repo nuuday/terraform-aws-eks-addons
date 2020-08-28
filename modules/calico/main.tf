779c3a2dec06ec04e38c26392a4148852c4b36ad
David Wolffberg <1350533+wolffberg@users.noreply.github.com>
2020-08-28 10:06:59 +0200

    fix: remove unused variables

--- modules/calico/main.tf
+++ modules/calico/main.tf
@@ -1,13 +1,3 @@
-locals {
-  values = {
-    calico = {
-      node = {
-        logseverity = "Debug"
-      }
-    }
-  }
-}
-
 resource "kubernetes_namespace" "this" {
   count = var.create_namespace ? 1 : 0
 
@@ -16,30 +6,15 @@ resource "kubernetes_namespace" "this" {
   }
 }
 
-# resource "null_resource" "apply_crds" {
-#   count = var.enable ? 1 : 0
-# 
-#   provisioner "local-exec" {
-#     command = <<EOF
-# cat <<MOF | kubectl \
-#   --insecure-skip-tls-verify \
-#   --token ${var.kubectl_token} \
-#   --server ${var.kubectl_server} \
-#     apply -k "${abspath(./crds)}"
-# MOF
-# EOF
-#   }
-# }
-
 resource "helm_release" "calico" {
   count      = var.enable ? 1 : 0
+
   name       = "aws-calico"
   chart      = "aws-calico"
   version    = var.chart_version
   repository = "https://aws.github.io/eks-charts"
   namespace  = var.namespace
   wait       = true
-  values     = [yamlencode(local.values)] 
 
   depends_on = [kubernetes_namespace.this]
 }

08436af0db7d7b6480ce24030f821d9738a06a10
wolffberg <1350533+wolffberg@users.noreply.github.com>
2020-08-28 10:47:13 +0200

    feat: add Calico addon (#64)

---
+++ modules/calico/variables.tf
@@ -0,0 +1,22 @@
+variable "enable" {
+  description = "Enable or disable Calico"
+  default     = true
+  type        = bool
+}
+
+variable "chart_version" {
+  description = "Helm chart version"
+  default     = "0.3.1"
+  type        = string
+}
+
+variable "create_namespace" {
+  description = "Whether to create the namespace defined in the namespace variable. Will fail if the namespace already exists."
+  default     = false
+}
+
+variable "namespace" {
+  description = "Namespace to deploy Calico in."
+  default     = "kube-system"
+  type        = string
+}

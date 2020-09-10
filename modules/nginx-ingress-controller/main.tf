cf1583b1e62dcec07a3106ce41c3d1eda8d164e9
Michael Kaiser <micka@nuuday.dk>
2020-09-10 13:57:34 +0200

    improve: add labe to filter ingress traffic in network policy

--- modules/nginx-ingress-controller/main.tf
+++ modules/nginx-ingress-controller/main.tf
@@ -106,6 +106,10 @@ resource "kubernetes_namespace" "this" {
   metadata {
     name = var.namespace
 
+    labels = {
+      role = "nginx_ingress"
+    }
+
     annotations = {
       managedby = "terraform"
     }
